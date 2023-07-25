0952c3b391f33cec6dd537034a9a3df6b5256b23
SwethaParished <swethaparished@gmail.com>
2023-07-25 17:26:34 -0500

    Done with the bonus mission

--- script.js
+++ script.js
@@ -5,19 +5,25 @@ window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then(function(data){
             console.log(data);
-            console.log(data.length);
+            
+            data.sort(function(a,b){
+               return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
+            })
+            container.innerHTML = `There are totally ${data.length} astronauts.`;
             for (let i=0; i<data.length; i++){
-            container.innerHTML = `<div class="astronaut">
-            <div class="bio">
-               <h3>${data[i].firstName} ${data[i].lastName}</h3>
-               <ul>
-                  <li>Hours in space: ${data[i].hoursInSpace}</li>
-                  <li>Active: ${data[i].active}</li>
-                  <li>Skills: ${data[i].skills}</li>
-               </ul>
-            </div>
-            <img class="avatar" src=${data[i].picture}>
-         </div>`
+                let activeClass = data[i].active ? "greenColor" : "";
+                
+                container.innerHTML += `<div class="astronaut">
+                <div class="bio">
+                    <h3>${data[i].firstName} ${data[i].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${data[i].hoursInSpace}</li>
+                        <li class="${activeClass}">Active: ${data[i].active}</li>
+                        <li>Skills: ${data[i].skills}</li>
+                    </ul>
+                    </div>
+                    <img class="avatar" src=${data[i].picture}>
+                </div>`
             }
         
 
